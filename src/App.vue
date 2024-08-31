33a867b0e3816da51ddd76215af4c0a9ebf7cb8e
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-08-31 18:24:51 -0300

    :sparkles: feat: add component/ Footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <Header />
   <Banner />
   <Main />
+  <Footer />
 </template>
 
 <script setup>
 import Header from "./components/Header/Header.vue";
 import Banner from "./components/Banner/Banner.vue";
 import Main from "./components/Main/Main.vue";
+import Footer from "./components/Footer/Footer.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer>
+    <p>
+      © Prime Video © Prime•Video © & ™ Aneis do Poder LTD © 2022 MARVERICK.
+      Reservados todos os direitos, Prime Video
+    </p>
+
+    <figure>
+      <img src="../../assets/image/youtube.svg" alt="Youtube" />
+      <img src="../../assets//image/insta.png" alt="Instagram" />
+      <img src="../../assets/image/prime.svg" alt="Prime Video" />
+    </figure>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 15vh;
+  flex-wrap: wrap;
+}
+
+figure {
+  display: flex;
+  justify-content: space-evenly;
+  width: 20vw;
+  max-width: 200px;
+
+  img {
+    width: 5vw;
+    max-width: 50px;
+  }
+}
+</style>
+
+<script setup></script>
