21e8140ece78b85b02a55369d16db4bea2985259
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-08-31 18:03:09 -0300

    :sparkles: feat: components/ banner

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Header />
+  <Banner />
 </template>
 
 <script setup>
 import Header from "./components/Header/Header.vue";
+import Banner from "./components/Banner/Banner.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+  <section></section>
+</template>
+
+<style scoped>
+section {
+  background-color: #1998ff;
+  height: 50vh;
+  background-image: url("../../assets/image/banner.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+</style>
+
+<script setup></script>
