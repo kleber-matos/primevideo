bc733e3fde8005da4cb54513baab2bc7aa6490cb
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-08-31 17:56:28 -0300

    :tada: Commit inicial

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Filmes</a></li>
+        <li><a href="#">Séries</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  background-color: black;
+}
+
+ul {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 50vw;
+  height: 10vh;
+}
+</style>
+
+<script setup></script>
