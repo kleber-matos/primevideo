1408a12db0d54ad78fec03d0852e56d8a2f9fd5a
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-08-31 18:16:12 -0300

    :sparkles: feat: add component/ Main

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Header />
   <Banner />
+  <Main />
 </template>
 
 <script setup>
 import Header from "./components/Header/Header.vue";
 import Banner from "./components/Banner/Banner.vue";
+import Main from "./components/Main/Main.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/Main/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <section>
+      <img src="../../assets/image/boys.png" alt="The Boys" />
+      <img src="../../assets/image/cris.png" alt="Todos mundo odeia o Chris" />
+      <img src="../../assets/image/strela.png" alt="InterStellar" />
+    </section>
+
+    <section>
+      <img src="../../assets/image/ring.png" alt="Rings of Power" />
+      <img src="../../assets/image/big.png" alt="The Big Bang" />
+      <img src="../../assets/image/guns.png" alt="Top Guns" />
+    </section>
+  </main>
+</template>
+
+<style scoped>
+section {
+  display: flex;
+  justify-content: space-evenly;
+  margin: 2rem 10% 2rem 10%;
+
+  img {
+    width: 15vw;
+    min-width: 150px;
+    height: 45vh;
+  }
+}
+</style>
+
+<script setup></script>
